2346d5a2520e8d4ba9ae6bf7e56fa89b560518b3
Alex Manoel Ferreira Silva <alex@legytma.com.br>
2020-07-26 15:25:09 -0300

    style: add support to eslint
    Add support to eslint.
    
    Signed-off-by: Alex Manoel Ferreira Silva <alex@legytma.com.br>

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -4,4 +4,6 @@ call npm install -g @adobe/jsonschema2md
 call npm install -g standard-version
 call npm install -g git-cz
 call npm install -g replace-in-file
+call npm install -g eslint
+call npm install -g eslint-config-google
 call npm install
